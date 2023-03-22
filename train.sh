5f7ac8f3eb0588a291b0dc5bdb76845b6c586e10
jerry zhao <jerryzhao173985@gmail.com>
2023-03-22 11:17:36 +0000

    add train.sh

---
+++ train.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+CUDA_VISIBLE_DEVICES=1 python3 main.py -O --text "a DSLR photo of cthulhu" --workspace trial_cthulhu --iters 200
+# CUDA_VISIBLE_DEVICES=1 python main.py -O --text "a DSLR photo of a squirrel" --workspace trial_squirrel --iters 200
+# CUDA_VISIBLE_DEVICES=1 python main.py -O --text "a DSLR photo of a cat lying on its side batting at a ball of yarn" --workspace trial_cat_lying --iters 200
